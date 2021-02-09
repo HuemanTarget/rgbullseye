8b66206e6e514564842e62ebf49b8af6de7cd52d
HuemanTarget <joshbasche@live.com>
2021-02-08 19:14:00 -0800

    first commit

---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Joshua Basche on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

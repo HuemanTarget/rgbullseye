bec03af7bc6bc68fd91adb9eb5eeda8ff1c4a37a
HuemanTarget <joshbasche@live.com>
2021-02-08 20:50:26 -0800

    add bind logic to red guess slider

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -14,28 +14,35 @@ struct ContentView: View {
     @State var gGuess: Double
     
     let rTarget = Double.random(in: 0..<1)
-    let bTarget = Double.random(in: 0..<1)
     let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
     
     // MARK:- BODY
     var body: some View {
         VStack {
             HStack {
                 VStack {
-                    Color(red: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, green: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, blue: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/)
+                    Color(red: rTarget, green: gTarget, blue: bTarget)
                     Text("Match this color")
                         .padding()
                 }//: VSTACK
                 VStack {
-                    Color(red: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, green: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, blue: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/)
-                    Text("R: 127 G: 127 B: 127")
+                    Color(red: rGuess, green: gGuess, blue: bGuess)
+                    Text("R: \(Int(rGuess * 255)) G: \(Int(gGuess * 255)) B: \(Int(bGuess * 255))")
                         .padding()
                 }//: VSTACK
             }//: HSTACK
             Button(action: {}) {
                 Text("Hit Me")
             }
-            Slider(value: .constant(0.5))
+            HStack {
+                Text("0")
+                    .foregroundColor(.red)
+                Slider(value: $rGuess)
+                Text("255")
+                    .foregroundColor(.red)
+            }//: HSTACK
+            .padding(.horizontal)
         }//: VSTACK
     }
 }
@@ -43,7 +50,7 @@ struct ContentView: View {
 // MARK:- PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(rGuess: 0.5, bGuess: 0.5, gGuess: 0.5)
             .previewLayout(.fixed(width: 568, height: 320))
     }
 }

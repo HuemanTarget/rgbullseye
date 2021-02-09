e86fbfd2d5563a95c4c53a45b11fd88c1a5a5d43
HuemanTarget <joshbasche@live.com>
2021-02-08 20:31:49 -0800

    add Color Button and Slider to ContentView

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK:- VARIABLES
+    @State var rGuess: Double
+    @State var bGuess: Double
+    @State var gGuess: Double
+    
+    let rTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    
+    // MARK:- BODY
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                VStack {
+                    Color(red: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, green: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, blue: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/)
+                    Text("Match this color")
+                        .padding()
+                }//: VSTACK
+                VStack {
+                    Color(red: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, green: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, blue: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/)
+                    Text("R: 127 G: 127 B: 127")
+                        .padding()
+                }//: VSTACK
+            }//: HSTACK
+            Button(action: {}) {
+                Text("Hit Me")
+            }
+            Slider(value: .constant(0.5))
+        }//: VSTACK
     }
 }
 
+// MARK:- PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
     }
 }
